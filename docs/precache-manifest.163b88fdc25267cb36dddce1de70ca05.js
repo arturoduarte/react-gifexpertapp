08dda96fb7d1f180f6d4b780bf540a4ccf432286
Arturo Duarte <informa.fj@gmail.com>
2020-09-24 19:35:27 -0400

    docs agregado

---
+++ docs/precache-manifest.163b88fdc25267cb36dddce1de70ca05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c3258478a2e2e38e435bd7a4700ca0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a07a6d22e5bf12974aa5",
+    "url": "/static/css/main.4e77ca32.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07a6d22e5bf12974aa5",
+    "url": "/static/js/main.4dd17d0b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
